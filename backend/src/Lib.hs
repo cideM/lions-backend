590b62a6874f45aa0cc68ded36990aced7dd0be4
Florian Beeres <yuuki@protonmail.com>
2021-07-20 12:01:12 +0200

    Formatting

--- backend/src/Lib.hs
+++ backend/src/Lib.hs
@@ -67,194 +67,195 @@ server ::
   Session.VaultKey ->
   InternalState ->
   Wai.ApplicationT m
-server
-  dbConn
-  sessionDataVaultKey
-  internalState
-  req
-  send = do
-    reqIdVaultKey <- asks App.getRequestIdVaultKey
+server dbConn sessionDataVaultKey internalState req send = do
+  reqIdVaultKey <- asks App.getRequestIdVaultKey
 
-    let vault = Wai.vault req
-        -- Create some helpers for doing things based on the user's auth status.
-        authInfo = Session.fromVault sessionDataVaultKey vault
+  let vault = Wai.vault req
 
-        -- TODO: Test this
-        adminOnlyOrOwn id next =
-          maybe send403 (next . (id,)) $ do
-            auth <- Session.getAuth authInfo
-            unless (Session.isAdmin authInfo) Nothing
-            let User.Session {..} = Session.get' auth
-            unless (sessionUserId /= id) Nothing
-            pure auth
+      -- Create some helpers for doing things based on the user's auth status.
+      authInfo = Session.fromVault sessionDataVaultKey vault
 
-        adminOnly' next = maybe send403 next (Session.getAdmin authInfo)
+      -- This function let's visitors access a resource if:
+      -- - They're an admin
+      -- - They're accessing a resource associated with their own user ID
+      -- TODO: Test this
+      adminOnlyOrOwn id next =
+        maybe send403 (next . (id,)) $ do
+          auth <- Session.getAuth authInfo
+          unless (Session.isAdmin authInfo) Nothing
+          let User.Session {..} = Session.get' auth
+          unless (sessionUserId /= id) Nothing
+          pure auth
 
-        authenticatedOnly' next = maybe send403 next (Session.getAuth authInfo)
+      adminOnly' next = maybe send403 next (Session.getAdmin authInfo)
 
-        -- The layout changes depending on whether you're logged in or not, so
-        -- we dependency inversion through partial application. Witness the
-        -- simplicity!
-        layout' = layout authInfo
+      authenticatedOnly' next = maybe send403 next (Session.getAuth authInfo)
 
-        requestId = maybe "" (Text.pack . show) $ Vault.lookup reqIdVaultKey vault
+      -- The layout changes depending on whether you're logged in or not, so
+      -- we dependency inversion through partial application. Witness the
+      -- simplicity!
+      layout' = layout authInfo
 
-        getUser = User.DB.getUser dbConn
-        -- Some helpers related to rendering content. I could look into
-        -- bringing back Snap or something similar so I don't need to
-        -- reimplement these helpers in a crappy and bug ridden way.
-        headers = [("Content-Type", "text/html; charset=UTF-8")]
-        render code = send . Wai.responseLBS code headers . renderBS
-        send200 = render status200
-        send403 = render status403 . layout' . LayoutStub "Fehler" Nothing $ warning "Du hast keinen Zugriff auf diese Seite"
-        send404 = render status404 . layout' . LayoutStub "Nicht gefunden" Nothing $ warning "Nicht Gefunden"
+      requestId = maybe "" (Text.pack . show) $ Vault.lookup reqIdVaultKey vault
 
-    K.katipAddContext (K.sl "request_id" requestId) $ do
-      K.logLocM K.InfoS "request received"
+      -- TODO: Remove
+      getUser = User.DB.getUser dbConn
 
-      case Wai.pathInfo req of
-        [] ->
-          case Wai.requestMethod req of
-            "GET" -> authenticatedOnly' $ WelcomeMessage.showFeed dbConn >=> send200 . layout'
-            _ -> send404
-        ["veranstaltungen"] ->
-          case Wai.requestMethod req of
-            "GET" -> authenticatedOnly' $ Events.Handlers.getAll >=> send200 . layout'
-            -- TODO: Send unsupported method 405
-            _ -> send404
-        ["veranstaltungen", "neu"] ->
-          case Wai.requestMethod req of
-            "GET" -> adminOnly' $ Events.Handlers.getCreate >=> send200 . layout'
-            "POST" ->
-              adminOnly' $ Events.Handlers.postCreate internalState req >=> send200 . layout'
-            -- TODO: Send unsupported method 405
-            _ -> send404
-        ["veranstaltungen", i] ->
-          case readEither (Text.unpack i) of
-            Left _ -> throwString . Text.unpack $ "couldn't parse route param for event ID as int: " <> i
-            Right (parsed :: Int) ->
-              case Wai.requestMethod req of
-                "GET" ->
-                  authenticatedOnly' $
-                    Events.Handlers.get (Events.Id parsed) >=> \case
-                      Nothing -> send404
-                      Just stub -> send200 $ layout' stub
-                _ -> send404
-        ["veranstaltungen", i, "antwort"] ->
-          case readEither (Text.unpack i) of
-            Left _ -> throwString . Text.unpack $ "couldn't parse route param for event ID as int: " <> i
-            Right (parsed :: Int) ->
-              case Wai.requestMethod req of
-                "POST" ->
-                  authenticatedOnly' $
-                    Events.Handlers.postReply getUser req send (Events.Id parsed)
-                _ -> send404
-        ["veranstaltungen", i, "loeschen"] ->
-          case readEither (Text.unpack i) of
-            Left _ -> throwString . Text.unpack $ "couldn't parse route param for event ID as int: " <> i
-            Right (parsed :: Int) ->
-              case Wai.requestMethod req of
-                "GET" -> adminOnly' $ Events.Handlers.getConfirmDelete (Events.Id parsed) >=> send200 . layout'
-                "POST" -> adminOnly' $ Events.Handlers.postDelete (Events.Id parsed) >=> send200 . layout'
-                _ -> send404
-        ["veranstaltungen", i, "editieren"] ->
-          case readEither (Text.unpack i) of
-            Left _ -> throwString . Text.unpack $ "couldn't parse route param for event ID as int: " <> i
-            Right (parsed :: Int) ->
-              case Wai.requestMethod req of
-                "GET" -> adminOnly' $ Events.Handlers.getEdit (Events.Id parsed) >=> send200 . layout'
-                "POST" ->
-                  adminOnly' $
-                    Events.Handlers.postUpdate internalState req (Events.Id parsed)
-                      >=> send200 . layout'
-                _ -> send404
-        ["loeschen", i] ->
-          case readEither (Text.unpack i) of
-            Left _ -> throwString . Text.unpack $ "couldn't parse route param for welcome message ID as int: " <> i
-            Right (parsed :: Int) ->
-              let msgId = WelcomeMsgId parsed
-               in case Wai.requestMethod req of
-                    "POST" -> adminOnly' $ WelcomeMessage.handleDeleteMessage dbConn msgId >=> send200 . layout'
-                    "GET" -> adminOnly' $ WelcomeMessage.showDeleteConfirmation dbConn msgId >=> send200 . layout'
-                    _ -> send404
-        ["neu"] ->
-          case Wai.requestMethod req of
-            "POST" -> adminOnly' $ WelcomeMessage.saveNewMessage dbConn req >=> send200 . layout'
-            "GET" -> adminOnly' $ WelcomeMessage.showAddMessageForm >=> send200 . layout'
-            _ -> send404
-        ["editieren", i] ->
-          case readEither (Text.unpack i) of
-            Left _ -> throwString . Text.unpack $ "couldn't parse route param for welcome message ID as int: " <> i
-            Right (parsed :: Int) ->
-              let msgId = WelcomeMsgId parsed
-               in case Wai.requestMethod req of
-                    "POST" -> adminOnly' $ WelcomeMessage.handleEditMessage dbConn req msgId >=> send200 . layout'
-                    "GET" -> adminOnly' $ WelcomeMessage.showMessageEditForm dbConn msgId >=> send200 . layout'
-                    _ -> send404
-        ["nutzer"] ->
-          case Wai.requestMethod req of
-            "GET" -> authenticatedOnly' $ Userlist.get dbConn req >=> send200 . layout'
-            _ -> send404
-        ["nutzer", "neu"] ->
-          case Wai.requestMethod req of
-            "POST" -> adminOnly' $ User.User.createPost dbConn req >=> send200 . layout'
-            "GET" -> adminOnly' (User.User.createGet >=> send200 . layout')
-            _ -> send404
-        ["nutzer", int, "editieren"] ->
-          case readEither (Text.unpack int) of
-            Left _ -> throwString . Text.unpack $ "couldn't parse route param for UserId as int: " <> int
-            Right (parsed :: Int) ->
-              let userId = UserId parsed
-               in case Wai.requestMethod req of
-                    "GET" -> adminOnlyOrOwn userId $
-                      \(id, auth) ->
-                        User.User.editGet dbConn id auth
-                          >>= send200 . layout'
-                    "POST" -> adminOnlyOrOwn userId $
-                      \(id, auth) ->
-                        User.User.editPost dbConn req id auth
-                          >>= send200 . layout'
-                    _ -> send404
-        ["nutzer", int] ->
-          case Wai.requestMethod req of
-            "GET" ->
-              case readEither (Text.unpack int) of
-                Left _ -> throwString . Text.unpack $ "couldn't parse route param for UserId as int: " <> int
-                Right (parsed :: Int) ->
-                  authenticatedOnly' $
-                    Userprofile.get dbConn parsed >=> \case
-                      Nothing -> send404
-                      Just stub -> send200 $ layout' stub
-            _ -> send404
-        ["nutzer", int, "loeschen"] ->
-          case readEither (Text.unpack int) of
-            Left _ -> throwString . Text.unpack $ "couldn't parse route param for UserId as int: " <> int
-            Right (parsed :: Int) ->
-              let userId = UserId parsed
-               in case Wai.requestMethod req of
-                    "GET" -> adminOnly' $ User.User.deleteGet dbConn userId >=> send200 . layout'
-                    "POST" -> adminOnly' $ User.User.deletePost dbConn userId >=> send200 . layout'
-                    _ -> send404
-        ["login"] ->
-          case Wai.requestMethod req of
-            "POST" -> Login.postLogin req send
-            "GET" -> Login.getLogin authInfo >>= send200
-            _ -> send404
-        ["logout"] ->
-          case Wai.requestMethod req of
-            "POST" -> Login.postLogout (Vault.lookup sessionDataVaultKey) req send
-            _ -> send404
-        ["passwort", "aendern"] ->
-          case Wai.requestMethod req of
-            "GET" -> Password.Change.Handlers.get req >>= send200 . layout'
-            "POST" -> Password.Change.Handlers.post req >>= send200 . layout'
-            _ -> send404
-        ["passwort", "link"] ->
-          case Wai.requestMethod req of
-            "GET" -> Password.Reset.Handlers.get >>= send200 . layout'
-            "POST" -> Password.Reset.Handlers.post req >>= send200 . layout'
-            _ -> send404
-        _ -> send404
+      -- Some helpers related to rendering content. I could look into
+      -- bringing back Snap or something similar so I don't need to
+      -- reimplement these helpers in a crappy and bug ridden way.
+      headers = [("Content-Type", "text/html; charset=UTF-8")]
+      render code = send . Wai.responseLBS code headers . renderBS
+      send200 = render status200
+      send403 = render status403 . layout' . LayoutStub "Fehler" Nothing $ warning "Du hast keinen Zugriff auf diese Seite"
+      send404 = render status404 . layout' . LayoutStub "Nicht gefunden" Nothing $ warning "Nicht Gefunden"
+
+  K.katipAddContext (K.sl "request_id" requestId) $ do
+    K.logLocM K.InfoS "request received"
+
+    case Wai.pathInfo req of
+      [] ->
+        case Wai.requestMethod req of
+          "GET" -> authenticatedOnly' $ WelcomeMessage.showFeed dbConn >=> send200 . layout'
+          _ -> send404
+      ["veranstaltungen"] ->
+        case Wai.requestMethod req of
+          "GET" -> authenticatedOnly' $ Events.Handlers.getAll >=> send200 . layout'
+          -- TODO: Send unsupported method 405
+          _ -> send404
+      ["veranstaltungen", "neu"] ->
+        case Wai.requestMethod req of
+          "GET" -> adminOnly' $ Events.Handlers.getCreate >=> send200 . layout'
+          "POST" ->
+            adminOnly' $ Events.Handlers.postCreate internalState req >=> send200 . layout'
+          -- TODO: Send unsupported method 405
+          _ -> send404
+      ["veranstaltungen", i] ->
+        case readEither (Text.unpack i) of
+          Left _ -> throwString . Text.unpack $ "couldn't parse route param for event ID as int: " <> i
+          Right (parsed :: Int) ->
+            case Wai.requestMethod req of
+              "GET" ->
+                authenticatedOnly' $
+                  Events.Handlers.get (Events.Id parsed) >=> \case
+                    Nothing -> send404
+                    Just stub -> send200 $ layout' stub
+              _ -> send404
+      ["veranstaltungen", i, "antwort"] ->
+        case readEither (Text.unpack i) of
+          Left _ -> throwString . Text.unpack $ "couldn't parse route param for event ID as int: " <> i
+          Right (parsed :: Int) ->
+            case Wai.requestMethod req of
+              "POST" ->
+                authenticatedOnly' $
+                  Events.Handlers.postReply getUser req send (Events.Id parsed)
+              _ -> send404
+      ["veranstaltungen", i, "loeschen"] ->
+        case readEither (Text.unpack i) of
+          Left _ -> throwString . Text.unpack $ "couldn't parse route param for event ID as int: " <> i
+          Right (parsed :: Int) ->
+            case Wai.requestMethod req of
+              "GET" -> adminOnly' $ Events.Handlers.getConfirmDelete (Events.Id parsed) >=> send200 . layout'
+              "POST" -> adminOnly' $ Events.Handlers.postDelete (Events.Id parsed) >=> send200 . layout'
+              _ -> send404
+      ["veranstaltungen", i, "editieren"] ->
+        case readEither (Text.unpack i) of
+          Left _ -> throwString . Text.unpack $ "couldn't parse route param for event ID as int: " <> i
+          Right (parsed :: Int) ->
+            case Wai.requestMethod req of
+              "GET" -> adminOnly' $ Events.Handlers.getEdit (Events.Id parsed) >=> send200 . layout'
+              "POST" ->
+                adminOnly' $
+                  Events.Handlers.postUpdate internalState req (Events.Id parsed)
+                    >=> send200 . layout'
+              _ -> send404
+      ["loeschen", i] ->
+        case readEither (Text.unpack i) of
+          Left _ -> throwString . Text.unpack $ "couldn't parse route param for welcome message ID as int: " <> i
+          Right (parsed :: Int) ->
+            let msgId = WelcomeMsgId parsed
+             in case Wai.requestMethod req of
+                  "POST" -> adminOnly' $ WelcomeMessage.handleDeleteMessage dbConn msgId >=> send200 . layout'
+                  "GET" -> adminOnly' $ WelcomeMessage.showDeleteConfirmation dbConn msgId >=> send200 . layout'
+                  _ -> send404
+      ["neu"] ->
+        case Wai.requestMethod req of
+          "POST" -> adminOnly' $ WelcomeMessage.saveNewMessage dbConn req >=> send200 . layout'
+          "GET" -> adminOnly' $ WelcomeMessage.showAddMessageForm >=> send200 . layout'
+          _ -> send404
+      ["editieren", i] ->
+        case readEither (Text.unpack i) of
+          Left _ -> throwString . Text.unpack $ "couldn't parse route param for welcome message ID as int: " <> i
+          Right (parsed :: Int) ->
+            let msgId = WelcomeMsgId parsed
+             in case Wai.requestMethod req of
+                  "POST" -> adminOnly' $ WelcomeMessage.handleEditMessage dbConn req msgId >=> send200 . layout'
+                  "GET" -> adminOnly' $ WelcomeMessage.showMessageEditForm dbConn msgId >=> send200 . layout'
+                  _ -> send404
+      ["nutzer"] ->
+        case Wai.requestMethod req of
+          "GET" -> authenticatedOnly' $ Userlist.get dbConn req >=> send200 . layout'
+          _ -> send404
+      ["nutzer", "neu"] ->
+        case Wai.requestMethod req of
+          "POST" -> adminOnly' $ User.User.createPost dbConn req >=> send200 . layout'
+          "GET" -> adminOnly' (User.User.createGet >=> send200 . layout')
+          _ -> send404
+      ["nutzer", int, "editieren"] ->
+        case readEither (Text.unpack int) of
+          Left _ -> throwString . Text.unpack $ "couldn't parse route param for UserId as int: " <> int
+          Right (parsed :: Int) ->
+            let userId = UserId parsed
+             in case Wai.requestMethod req of
+                  "GET" -> adminOnlyOrOwn userId $
+                    \(id, auth) ->
+                      User.User.editGet dbConn id auth
+                        >>= send200 . layout'
+                  "POST" -> adminOnlyOrOwn userId $
+                    \(id, auth) ->
+                      User.User.editPost dbConn req id auth
+                        >>= send200 . layout'
+                  _ -> send404
+      ["nutzer", int] ->
+        case Wai.requestMethod req of
+          "GET" ->
+            case readEither (Text.unpack int) of
+              Left _ -> throwString . Text.unpack $ "couldn't parse route param for UserId as int: " <> int
+              Right (parsed :: Int) ->
+                authenticatedOnly' $
+                  Userprofile.get dbConn parsed >=> \case
+                    Nothing -> send404
+                    Just stub -> send200 $ layout' stub
+          _ -> send404
+      ["nutzer", int, "loeschen"] ->
+        case readEither (Text.unpack int) of
+          Left _ -> throwString . Text.unpack $ "couldn't parse route param for UserId as int: " <> int
+          Right (parsed :: Int) ->
+            let userId = UserId parsed
+             in case Wai.requestMethod req of
+                  "GET" -> adminOnly' $ User.User.deleteGet dbConn userId >=> send200 . layout'
+                  "POST" -> adminOnly' $ User.User.deletePost dbConn userId >=> send200 . layout'
+                  _ -> send404
+      ["login"] ->
+        case Wai.requestMethod req of
+          "POST" -> Login.postLogin req send
+          "GET" -> Login.getLogin authInfo >>= send200
+          _ -> send404
+      ["logout"] ->
+        case Wai.requestMethod req of
+          "POST" -> Login.postLogout (Vault.lookup sessionDataVaultKey) req send
+          _ -> send404
+      ["passwort", "aendern"] ->
+        case Wai.requestMethod req of
+          "GET" -> Password.Change.Handlers.get req >>= send200 . layout'
+          "POST" -> Password.Change.Handlers.post req >>= send200 . layout'
+          _ -> send404
+      ["passwort", "link"] ->
+        case Wai.requestMethod req of
+          "GET" -> Password.Reset.Handlers.get >>= send200 . layout'
+          "POST" -> Password.Reset.Handlers.post req >>= send200 . layout'
+          _ -> send404
+      _ -> send404
 
 main :: IO ()
 main = do
