39681223a1d02bcdeafba09b9fc10ff82bf90042
Florian Beeres <yuuki@protonmail.com>
2023-01-18 15:19:16 +0100

    Style changes

--- backend/src/Session/Middleware.hs
+++ backend/src/Session/Middleware.hs
@@ -1,9 +1,10 @@
-module Session.Middleware (middleware) where
+module Session.Middleware where
 
 import qualified App
 import Control.Exception.Safe
 import Control.Monad.IO.Class (MonadIO)
 import Control.Monad.Reader.Class (MonadReader, asks)
+import Data.Function ((&))
 import Data.Text.Encoding (decodeUtf8)
 import qualified Data.Vault.Lazy as Vault
 import qualified Katip as K
@@ -18,10 +19,8 @@ import qualified Web.ClientSession as ClientSession
 import qualified Web.Cookie as Cookie
 import Prelude hiding (id, log)
 
--- This is the main user facing export. This middleware will try to log a user
--- in based on cookie information. The login information is then persisted in
--- the Vault associated with this particular request so that downstream code
--- can access it.
+-- Try to log the user in based on the session ID found in a specific cookie.
+-- If not, redirect to /login
 middleware ::
   ( MonadIO m,
     K.KatipContext m,
@@ -33,8 +32,8 @@ middleware ::
   ) =>
   Wai.MiddlewareT m
 middleware nextApp req send = do
-  maybeRequestWithSession <- login req
-  case maybeRequestWithSession of
+  mbReqWithVault <- login
+  case mbReqWithVault of
     Nothing ->
       -- The user doesn't have a valid session. To prevent infinite redirects,
       -- we need to match on the route.
@@ -47,41 +46,33 @@ middleware nextApp req send = do
           nextApp req send
         _ -> send $ Wai.responseBuilder status302 [("Location", "/login")] ""
     Just req' -> nextApp req' send
+  where
+    login = do
+      vaultKey <- asks App.getSessionDataVaultKey
+      encKey <- asks App.getSessionEncryptionKey
 
--- login adds the user session to the vault, and adds the vault to the request,
--- in case the user has a valid session. If not, the original request is
--- returned unchanged.
-login ::
-  ( MonadIO m,
-    MonadReader env m,
-    MonadThrow m,
-    App.HasDb env,
-    App.HasSessionEncryptionKey env,
-    App.HasSessionDataVaultKey env
-  ) =>
-  Wai.Request ->
-  m (Maybe Wai.Request)
-login req = do
-  sessionDataVaultKey <- asks App.getSessionDataVaultKey
-  sessionKey <- asks App.getSessionEncryptionKey
-
-  case (lookup "cookie" $ Wai.requestHeaders req)
-    >>= lookup "lions_session" . Cookie.parseCookies
-    >>= ClientSession.decrypt sessionKey of
-    Nothing -> return Nothing
-    Just sessionIdDecrypted -> do
-      maybeSession <- Session.get (Session.Id $ decodeUtf8 sessionIdDecrypted)
-      case maybeSession of
+      let mbSessionId = getSessionId encKey req
+      case mbSessionId of
         Nothing -> return Nothing
-        Just session@(Session _ _ userId) -> do
-          tryParse <- Session.Valid.parse session
-          case tryParse of
-            -- The error here just says that the session expired
-            Left _ -> return Nothing
-            Right _ -> do
-              maybeRoles <- Role.get userId
-              case maybeRoles of
-                Nothing -> return Nothing
-                Just roles -> do
-                  let vault' = Vault.insert sessionDataVaultKey (roles, userId) $ Wai.vault req
-                  return . Just $ req {Wai.vault = vault'}
+        Just sessionId -> do
+          mbSession <- Session.get sessionId
+          case mbSession of
+            Nothing -> return Nothing
+            Just session -> do
+              mbValid <- Session.Valid.parse session
+              case mbValid of
+                Left _ -> return Nothing
+                Right valid -> do
+                  let Session _ _ userId = Session.Valid.unvalid valid
+                  mbRoles <- Role.get userId
+                  case mbRoles of
+                    Nothing -> return Nothing
+                    Just roles -> do
+                      let vault' = Vault.insert vaultKey (roles, userId) $ Wai.vault req
+                      return . Just $ req {Wai.vault = vault'}
+
+    getSessionId encKey request = do
+      cookies <- Wai.requestHeaders request & lookup "cookie"
+      sessionId <- lookup "lions_session" $ Cookie.parseCookies cookies
+      sessionIdDecrypted <- ClientSession.decrypt encKey sessionId
+      return . Session.Id $ decodeUtf8 sessionIdDecrypted
