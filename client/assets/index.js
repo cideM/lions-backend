55ab0d9cc8d9a8217043ec6c15bd4fda6fa085db
Florian Beeres <yuuki@protonmail.com>
2023-02-08 15:08:55 +0100

    Fix error in JS for Safari
    Don't create function in blocks, so no "function foo() {}" inside an if
    () {}

--- client/assets/index.js
+++ client/assets/index.js
@@ -1,55 +1,59 @@
 const $emailButton = document.getElementById("email-button");
+
+const selected = new Set();
+let $checkboxes;
+
+function updateEmailButton() {
+  $emailButton.href = "mailto:" + Array.from(selected).sort().join(",");
+  if (selected.size > 0) {
+    $emailButton.classList.remove("disabled");
+  } else {
+    $emailButton.classList.add("disabled");
+  }
+}
+
+function onClickToggle() {
+  switch (selected.size) {
+    case $checkboxes.length: {
+      $checkboxes.forEach(($cb) => {
+        $cb.checked = false;
+        selected.clear();
+      });
+      break;
+    }
+    case 0:
+      $checkboxes.forEach(($cb) => {
+        $cb.checked = true;
+        selected.add($cb.dataset.email);
+      });
+      break;
+    default:
+      $checkboxes.forEach(($cb) => {
+        $cb.checked = true;
+        selected.add($cb.dataset.email);
+      });
+      break;
+  }
+  updateEmailButton();
+}
+
+function onClickCheckbox(event) {
+  const $el = event.currentTarget;
+  if ($el.checked) {
+    selected.add($el.dataset.email);
+  } else {
+    selected.delete($el.dataset.email);
+  }
+  updateEmailButton();
+}
+
 if ($emailButton) {
-  const $checkboxes = document.querySelectorAll("[data-email]");
-  const $toggleButton = document.getElementById("toggle-email-button");
+  $checkboxes = document.querySelectorAll("[data-email]");
 
-  const selected = new Set();
+  document
+    .getElementById("toggle-email-button")
+    .addEventListener("click", onClickToggle);
 
-  function updateEmailButton() {
-    $emailButton.href = "mailto:" + Array.from(selected).sort().join(",");
-    if (selected.size > 0) {
-      $emailButton.classList.remove("disabled");
-    } else {
-      $emailButton.classList.add("disabled");
-    }
-  }
-
-  function onClickToggle() {
-    switch (selected.size) {
-      case $checkboxes.length: {
-        $checkboxes.forEach(($cb) => {
-          $cb.checked = false;
-          selected.clear();
-        });
-        break;
-      }
-      case 0:
-        $checkboxes.forEach(($cb) => {
-          $cb.checked = true;
-          selected.add($cb.dataset.email);
-        });
-        break;
-      default:
-        $checkboxes.forEach(($cb) => {
-          $cb.checked = true;
-          selected.add($cb.dataset.email);
-        });
-        break;
-    }
-    updateEmailButton();
-  }
-
-  function onClickCheckbox(event) {
-    const $el = event.currentTarget;
-    if ($el.checked) {
-      selected.add($el.dataset.email);
-    } else {
-      selected.delete($el.dataset.email);
-    }
-    updateEmailButton();
-  }
-
-  $toggleButton.addEventListener("click", onClickToggle);
   $checkboxes.forEach(($cb) => {
     $cb.addEventListener("click", onClickCheckbox);
   });
